d17a7697b3d09dd400a983292225e932e33de918
Mayank <7silenteyes07@gmail.com>
2023-04-15 17:10:02 +0530

    cc

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image ,View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image
+      style={{width: '100%'}}
+        resizeMode='contain'
+        source={require('./assets/chat1.png')} 
+      />
       <StatusBar style="auto" />
     </View>
   );
