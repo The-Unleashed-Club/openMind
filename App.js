394d6cbee706695e4043502ebfbdd69842aa0c66
nandini25-ag <64517220+nandini25-ag@users.noreply.github.com>
2023-04-16 01:17:14 +0530

    login

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Image ,View} from 'react-native';
-
+import Login from "./Screens/login";
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Image
-      style={{width: '100%'}}
-        resizeMode='contain'
-        source={require('./assets/chat1.png')} 
+        style={{ width: "100%" }}
+        resizeMode="contain"
+        source={require("./assets/chat1.png")}
       />
       <StatusBar style="auto" />
+      <Login />
     </View>
   );
 }
---
+++ Screens/login.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
+
+const Login = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = () => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.loginLabel}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Text style={styles.forgotPassword}>Forgot Password?</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 25,
+  },
+  loginLabel: {
+    fontSize: 50,
+    fontWeight: "bold",
+    marginBottom: 24,
+    color: "#333",
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    marginBottom: 16,
+    backgroundColor: "#FFFFFF",
+    fontSize: 22,
+    color: "#333",
+  },
+  forgotPassword: {
+    marginTop: 16,
+    color: "#555",
+    textDecorationLine: "underline",
+  },
+  Button: {
+    width: "100%",
+    height: 48,
+    borderRadius: 8,
+    backgroundColor: "#007AFF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Login;
