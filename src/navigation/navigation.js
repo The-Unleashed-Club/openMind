655445ca820887b8e727e789e7b03e740150b28a
Mayank <7silenteyes07@gmail.com>
2023-06-19 23:52:26 +0530

    nav-cc

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -71,16 +71,16 @@ export default function Home() {
   }, []);
 
 
-  ////  Comment Below stack for Development Mode /////
-  ////  UnComment Below stack for Production Mode /////
+  //  Comment Below stack for Development Mode /////
+  //  UnComment Below stack for Production Mode /////
 
-      // return (
-      //   <NavigationContainer>
-      //     {isLoading ? (
-      //       <Loading_Screen />
-      //     ) : user ? ( <AppScreens />) : ( <AuthScreens />)}
-      //   </NavigationContainer>
-      //   );
+      return (
+        <NavigationContainer>
+          {isLoading ? (
+            <Loading_Screen />
+          ) : user ? ( <AppScreens />) : ( <AuthScreens />)}
+        </NavigationContainer>
+        );
 
   
 
