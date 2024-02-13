d6cbd8d7c3ae83c52514437cbef419c4023a129a
Yogeshwar Chaudhari <chaudhari.yogesh20@gmail.com>
2024-02-13 21:00:29 +1030

    signup form working as usual.

--- src/Screens/SignupScreen.tsx
+++ src/Screens/SignupScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react";
-import { View, StyleSheet, Text, Alert } from "react-native";
+import { View, StyleSheet, Text, Alert, KeyboardAvoidingView, Keyboard, Platform, TouchableWithoutFeedback } from "react-native";
 import {
   auth,
   createUserWithEmailAndPassword,
@@ -9,14 +9,21 @@ import { setLoading } from "../state-managment/reducers";
 import { useDispatch } from "react-redux";
 import { db, collection, addDoc } from "../firebase/firebase-utilities";
 import { TextInput } from "@/components/input/TextInput";
-import { useForm } from "react-hook-form"
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type SignupFormData = {
+  name: string;
+  email: string;
+  password: string;
+  rePassword: string;
+}
 
 export const useSignupScreen = () => {
   const dispatch = useDispatch();
   const {
     control,
     handleSubmit,
-  } = useForm({
+  } = useForm<SignupFormData>({
     defaultValues: {
       name: "",
       email: "",
@@ -25,27 +32,10 @@ export const useSignupScreen = () => {
     },
   })
 
-  const onSignupFormSubmit = useCallback(() => { }, [])
+  const onSignupFormSubmit: SubmitHandler<SignupFormData> = useCallback((data) => {
+    const { name, email, password, rePassword } = data;
 
-  return {
-    control,
-    handleSubmit,
-    onSignupFormSubmit
-  }
-}
-
-export const SignupScreen = () => {
-  const { control,
-    handleSubmit,
-    onSignupFormSubmit } = useSignupScreen();
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [RePassword, setRePassword] = React.useState("");
-  const [name, setName] = React.useState("");
-  const dispatch = useDispatch();
-
-  const handleSignUp = () => {
-    if (password != RePassword) {
+    if (password !== rePassword) {
       Alert.alert("Password Do not match, please try again");
     } else {
       dispatch(setLoading(true));
@@ -71,49 +61,68 @@ export const SignupScreen = () => {
         dispatch(setLoading(false));
       }
     }
-  };
+  }, [])
 
+  return {
+    control,
+    handleSubmit,
+    onSignupFormSubmit
+  }
+}
+
+export const SignupScreen = () => {
+  const { control,
+    handleSubmit,
+    onSignupFormSubmit } = useSignupScreen();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.container1}>
-        <Text style={styles.SignUpLabel}>SignUp</Text>
-      </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={styles.container1}>
+            <Text style={styles.SignUpLabel}>SignUp</Text>
+          </View>
 
-      <View style={{ width: "100%" }}>
-        <TextInput control={control} name="name" label={"Your Name"} rules={{
-          required: {
-            value: true,
-            message: "Please enter your name",
-          },
-        }} />
+          <View style={{ width: "100%" }}>
+            <TextInput control={control} name="name" label={"Your Name"} rules={{
+              required: {
+                value: true,
+                message: "Please enter your name",
+              },
+            }} />
 
-        <TextInput control={control} name="email" label={"Your Email"} rules={{
-          required: {
-            value: true,
-            message: "Please enter your email address",
-          },
-        }} />
+            <TextInput control={control} name="email" label={"Your Email"} rules={{
+              required: {
+                value: true,
+                message: "Please enter your email address",
+              },
+            }} />
 
-        <TextInput control={control} name="password" label={"Enter Password"} rules={{
-          required: {
-            value: true,
-            message: "Password is required",
-          },
-        }} />
+            <TextInput control={control} name="password" label={"Enter Password"} rules={{
+              required: {
+                value: true,
+                message: "Password is required",
+              },
+            }} />
 
-        <TextInput control={control} name="rePassword" label={"Re-enter Password"} rules={{
-          required: {
-            value: true,
-            message: "Please re-enter your password",
-          },
-        }} />
-      </View>
+            <TextInput control={control} name="rePassword" label={"Re-enter Password"} rules={{
+              required: {
+                value: true,
+                message: "Please re-enter your password",
+              },
+            }} />
+          </View>
 
-      <View style={styles.container2}>
-        <Button_1 title="SignUp" onPress={handleSubmit(onSignupFormSubmit)} />
-      </View>
-    </View>
+          <View style={styles.container2}>
+            <Button_1 title="SignUp" onPress={handleSubmit(onSignupFormSubmit)} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+
   );
 };
 
@@ -131,7 +140,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignSelf: "center",
     top: "10%",
-    // backgroundColor: "red",
   },
   container2: {
     width: "100%",
@@ -142,13 +150,7 @@ const styles = StyleSheet.create({
   SignUpLabel: {
     fontSize: 50,
     marginBottom: 24,
-    fontFamily: "OpenSans_800ExtraBold",
     color: "#224957",
-  },
-  forgotPassword: {
-    marginTop: 16,
-    color: "#555",
-    textDecorationLine: "underline",
   },
   Button: {
     width: "100%",
