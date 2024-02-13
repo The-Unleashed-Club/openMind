89abc22e63f806f1616794dd4b2a093904fe75fc
Yogeshwar Chaudhari <chaudhari.yogesh20@gmail.com>
2024-02-13 20:57:40 +1030

    removed font loading since it is causing unnessary warnings

--- App.tsx
+++ App.tsx
@@ -5,21 +5,6 @@ import Home from "./src/navigation/navigation";
 import { Provider } from "react-redux";
 import store from "./src/state-managment/store";
 import LoadingScreen from '@/Screens/LoadingScreen';
-import {
-  useFonts,
-  OpenSans_300Light,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_600SemiBold,
-  OpenSans_700Bold,
-  OpenSans_800ExtraBold,
-  OpenSans_300Light_Italic,
-  OpenSans_400Regular_Italic,
-  OpenSans_500Medium_Italic,
-  OpenSans_600SemiBold_Italic,
-  OpenSans_700Bold_Italic,
-  OpenSans_800ExtraBold_Italic,
-} from '@expo-google-fonts/open-sans';
 import { PaperProvider } from 'react-native-paper';
 import { MD3LightTheme as DefaultTheme } from 'react-native-paper';
 
@@ -43,21 +28,6 @@ LogBox.ignoreLogs([
 
 export default function App() {
 
-  let [fontsLoaded] = useFonts({
-    OpenSans_300Light,
-    OpenSans_400Regular,
-    OpenSans_500Medium,
-    OpenSans_600SemiBold,
-    OpenSans_700Bold,
-    OpenSans_800ExtraBold,
-    OpenSans_300Light_Italic,
-    OpenSans_400Regular_Italic,
-    OpenSans_500Medium_Italic,
-    OpenSans_600SemiBold_Italic,
-    OpenSans_700Bold_Italic,
-    OpenSans_800ExtraBold_Italic,
-  });
-
   const theme = {
     ...DefaultTheme,
     colors: {
@@ -67,20 +37,14 @@ export default function App() {
     },
   };
 
-  if (!fontsLoaded) {
-    return < LoadingScreen />;
-  } else {
-
-    // please check developer mode in navigation.js before continuing.
-    return (
-      <Provider store={store}>
-        <PaperProvider theme={theme}>
-          <Home />
-        </PaperProvider>
-      </Provider>
-    );
-  }
-
+  // please check developer mode in navigation.js before continuing.
+  return (
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <Home />
+      </PaperProvider>
+    </Provider>
+  );
 }
 
 
