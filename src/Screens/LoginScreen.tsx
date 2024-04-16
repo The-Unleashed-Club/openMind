962e63fc863920fa1229be9514fb901d31cddd6b
MechanikGamer <123777101+MechanikGamer@users.noreply.github.com>
2024-04-16 13:40:06 +0200

    Added error handling as requested on issue on Github
    This PR addresses the need for better error handling during the login process, as discussed in issue #37. The changes introduced here aim to provide clear, user-friendly feedback for various authentication errors returned by Firebase during login attempts.
    
    ### Changes:
    - Enhanced error handling in the `onLoginFormSubmit` function within the `useLoginScreen` hook.
    - Error messages are now set based on the specific error codes returned by Firebase:
      - `auth/invalid-email`: Informs the user that the email address format is incorrect.
      - `auth/wrong-password`: Alerts the user that the entered password is incorrect.
      - `auth/user-not-found`: Notifies the user when there is no account associated with the entered email.
      - All other errors: A generic message is shown to cover any other unexpected errors.
    
    These messages are displayed directly on the `LoginScreen`, enhancing the user experience by providing immediate, relevant feedback for correction or understanding of the issue at hand.
    
    ### Motivation:
    The need for clearer error messages was highlighted in issue #37 where it was noted that users were facing difficulties understanding the reasons behind authentication failures. This enhancement aims to resolve this by explicitly handling each error scenario.
    
    ### Testing:
    - Manual tests were performed to ensure that the appropriate messages are displayed for different error scenarios.
    
    This update should make the login process more intuitive and less frustrating for users by clearly communicating what needs to be fixed or what issue might have occurred. Please review the changes and let me know if further modifications are required.
    
    Thank you!

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -1,94 +1,133 @@
-import React, { useCallback } from "react";
-import { View, StyleSheet, Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Text,
+} from "react-native";
 import { useDispatch } from "react-redux";
 import { auth, signInWithEmailAndPassword } from "../firebase-utilities";
 import { setLoading, setUser } from "../state-managment/reducers";
-import { useForm, SubmitHandler } from "react-hook-form"
-import { Text, Button } from "react-native-paper"
+import { useForm, SubmitHandler } from "react-hook-form";
 import { TextInput } from "@/components/input/TextInput";
+import { Button } from "react-native-paper";
 
 type LoginFormData = {
   email: string;
   password: string;
-}
+};
 
 export const useLoginScreen = () => {
   const dispatch = useDispatch();
+  const [error, setError] = useState<string | null>(null);
+
   const {
     control,
     handleSubmit,
+    formState: { errors },
   } = useForm<LoginFormData>({
     defaultValues: {
       email: "",
       password: "",
     },
-  })
+  });
 
-  const onLoginFormSubmit: SubmitHandler<LoginFormData> = useCallback(async (data) => {
-    dispatch(setLoading(true));
-    const { email, password } = data
-
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      dispatch(setUser(userCredential.user));
-    } catch (error) {
-      console.error(error.message);
-      dispatch(setLoading(false));
-    }
-  }, [dispatch, signInWithEmailAndPassword])
+  const onLoginFormSubmit: SubmitHandler<LoginFormData> = useCallback(
+    async (data) => {
+      setError(null);
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          data.email,
+          data.password
+        );
+        dispatch(setUser(userCredential.user));
+      } catch (error: any) {
+        console.error(error.message);
+        if (error.code) {
+          switch (error.code) {
+            case "auth/invalid-email":
+              setError("The email address is badly formatted.");
+              break;
+            case "auth/wrong-password":
+              setError("Wrong password. Please try again.");
+              break;
+            case "auth/user-not-found":
+              setError("No user found with this email.");
+              break;
+            default:
+              setError("An unexpected error occurred. Please try again.");
+              break;
+          }
+        } else {
+          setError("A network error occurred. Please try again later.");
+        }
+      }
+    },
+    [dispatch]
+  );
 
   return {
     control,
     handleSubmit,
-    onLoginFormSubmit
-  }
-}
+    onLoginFormSubmit,
+    error,
+  };
+};
 
 export const LoginScreen = () => {
-  const { control,
-    handleSubmit,
-    onLoginFormSubmit } = useLoginScreen();
+  const { control, handleSubmit, onLoginFormSubmit, error } = useLoginScreen();
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ flex: 1 }}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
           <View>
-            <Text style={{
-              fontSize: 36,
-              fontWeight: "bold"
-            }}>Welcome Back!</Text>
-            <Text variant="bodySmall">We're glad to see you back.</Text>
+            <Text style={{ fontSize: 36, fontWeight: "bold" }}>
+              Welcome Back!
+            </Text>
+            <Text>We're glad to see you back.</Text>
+          </View>
+          <View style={{ marginVertical: 50, gap: 8 }}>
+            <TextInput
+              control={control}
+              name="email"
+              label={"Your Email"}
+              rules={{
+                required: {
+                  value: true,
+                  message: "Please enter your email address",
+                },
+              }}
+            />
+            <TextInput
+              secureTextEntry
+              control={control}
+              name="password"
+              label={"Your Password"}
+              rules={{
+                required: {
+                  value: true,
+                  message: "Please enter your password",
+                },
+              }}
+            />
+            {error && (
+              <Text style={{ color: "red", marginVertical: 8 }}>{error}</Text>
+            )}
           </View>
 
-          <View style={{
-            marginVertical: 50,
-            gap: 8
-          }}>
-            <TextInput control={control} name="email" label={"Your Email"} rules={{
-              required: {
-                value: true,
-                message: "Please enter your email address",
-              },
-            }} />
-
-            <TextInput secureTextEntry control={control} name="password" label={"Your Password"} rules={{
-              required: {
-                value: true,
-                message: "Please enter your password",
-              },
-            }} />
+          <View style={{ gap: 8 }}>
+            <Button mode="contained" onPress={handleSubmit(onLoginFormSubmit)}>
+              Login
+            </Button>
           </View>
-
-          <View style={{
-            gap: 8
-          }}>
-            <Button mode="contained" onPress={handleSubmit(onLoginFormSubmit)}>Login</Button>
-          </View>
-
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
@@ -102,5 +141,5 @@ const styles = StyleSheet.create({
     paddingVertical: 75,
     paddingLeft: 24,
     paddingRight: 24,
-  }
+  },
 });
